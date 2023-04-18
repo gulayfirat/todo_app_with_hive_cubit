a8ec5008720fa31584deb4c7af8f7640fa1ec732
gulayfirat <guleturcan@gmail.com>
2023-04-18 10:51:45 +0300

    (issue) Adding documents to generic classes

--- lib/core/base/base_cache_manager.dart
+++ lib/core/base/base_cache_manager.dart
@@ -2,9 +2,11 @@ import 'package:hive/hive.dart';
 
 abstract class BaseCacheManager<T> {
   final String key;
+  //T type the hive box
   Box<T>? box;
 
   BaseCacheManager(this.key);
+  //Initial manager
   Future<void> init() async {
     registerAdapters();
     if (!(box?.isOpen ?? false)) {
@@ -12,18 +14,22 @@ abstract class BaseCacheManager<T> {
     }
   }
 
+  //Register hive adapters
   void registerAdapters();
-
+  //Delete all items from hive
   Future<void> deleteAllItems() async {
     await box?.deleteFromDisk();
   }
 
+//Add all items to hive
   Future<void> addItems(List<T> items);
   Future<void> putItems(List<T> items);
-
+//Just fetches an item from hive
   T? getItem(String key);
+//Fetches all items from hive
   Future<List<T>?> getItems();
-
+//Add or update item to hive
   Future<void> putItem(String key, T item);
+//Delete just an item from hive
   Future<void> removeItem(String key);
 }
--- lib/core/base/base_model.dart
+++ lib/core/base/base_model.dart
@@ -1,6 +1,8 @@
-
 abstract class BaseModel<T> {
+  //Converts an object to a Map data structure
   Map<String, dynamic>? toJson();
+  // Create a new Object T type
   T createObject();
+  //Creates an object from the Map data structure
   T fromJson(Map<String, dynamic> json);
 }
